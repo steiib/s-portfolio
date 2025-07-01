4996af21c239b063118b32ddc49a651c088990ac
Steve Andrei <sfajilan27@gmail.com>
2025-07-01 21:17:25 +0800

    feat: add about section

--- src/content.tsx
+++ src/content.tsx
@@ -12,14 +12,16 @@ function Content() {
     document.body.classList.toggle("bg-default", !toggled);
   }, [toggled]);
 
-  /* Name Placeholder */
   return (
     <>
       <Navbar toggled={toggled} />
-      <div
+
+      {/* Home Section */}
+      <section
+        id="home"
         className={`${
           toggled ? "text-black" : "text-white"
-        } w-11/12 max-w-sm mx-auto text-center md:max-w-3xl`}
+        } scroll-mt-40 w-11/12 max-w-sm mx-auto text-center md:max-w-3xl`}
       >
         <p className="text-xl md:text-4xl font-semibold mt-10">Hi, I'm</p>
         <img
@@ -27,11 +29,26 @@ function Content() {
           alt="NameBG"
           className="w-full h-auto mx-auto block md:mt-3 md:mb-3"
         />
-        <div className="text-2xl md:text-5xl font-semibold tracking-widest">
+        <div className="text-3xl md:text-6xl font-semibold tracking-widest">
           <p>S T E V E</p>
           <p>F A J I L A N</p>
         </div>
-      </div>
+      </section>
+
+      {/* About Section */}
+      <section
+        id="about"
+        className={`${
+          toggled ? "text-black" : "text-white"
+        } scroll-mt-40 w-11/12 max-w-sm mx-auto text-center mt-20 md:mt-40 md:max-w-3xl`}
+      >
+        <div className="text-xl md:text-3xl font-semibold">
+          <p>About Me</p>
+        </div>
+        {/* Add more about content here */}
+      </section>
+
+      {/* You can add more sections like projects and contact here */}
 
       <Button toggled={toggled} setToggled={setToggled} />
     </>
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Content from "./content.tsx";
 import "./index.css";
+import Navbar from "./navbar.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
