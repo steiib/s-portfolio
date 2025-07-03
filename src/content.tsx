97f72790576cc97eacfea625fa824ff1753879e3
Steve Andrei <sfajilan27@gmail.com>
2025-07-03 18:00:36 +0800

    feat: update about me section

--- src/content.tsx
+++ src/content.tsx
@@ -23,13 +23,13 @@ function Content() {
           toggled ? "text-black" : "text-white"
         } scroll-mt-40 w-11/12 max-w-sm mx-auto text-center md:max-w-3xl`}
       >
-        <p className="text-xl md:text-4xl font-semibold mt-10">Hi, I'm</p>
+        <p className="text-xl md:text-4xl font-bold mt-10">Hi, I'm</p>
         <img
           src={toggled ? topo_b : topo_w}
           alt="NameBG"
           className="w-full h-auto mx-auto block md:mt-3 md:mb-3"
         />
-        <div className="text-3xl md:text-6xl font-semibold tracking-widest">
+        <div className="text-3xl md:text-6xl font-extrabold tracking-widest cursor-pointer hover:scale-120 transition-all">
           <p>S T E V E</p>
           <p>F A J I L A N</p>
         </div>
@@ -40,10 +40,21 @@ function Content() {
         id="about"
         className={`${
           toggled ? "text-black" : "text-white"
-        } scroll-mt-40 w-11/12 max-w-sm mx-auto text-center mt-20 md:mt-40 md:max-w-3xl`}
+        } scroll-mt-40 w-11/12 max-w-sm mx-auto text-center mt-20 md:mt-40 md:max-w-4xl`}
       >
-        <div className="text-xl md:text-3xl font-semibold">
+        <div className="text-2xl md:text-4xl font-bold">
           <p>About Me</p>
+        </div>
+        <div className="text-sm md:text-2xl font-light mt-4 md:mt-8">
+          <p>
+            I am a 4th-year Computer Engineering student at FEU Institute of
+            Technology. I enjoy working on the creative side of software
+            development, especially front-end projects where I can focus on
+            design and user experience.
+          </p>
+        </div>
+        <div className="md:text-3xl mt-8 md:mt-16 font-semibold">
+          <p>Languages</p>
         </div>
       </section>
 
